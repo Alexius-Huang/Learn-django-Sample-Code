332963f003b81eab54d0244e883cbf3bc91540d1
Maxwell-Alexius <destinydragon0614@yahoo.com.tw>
2016-07-25 13:40:26 +0800

    Add more views including 'polls.detail', 'polls.results' and 'polls.vote' / Add url pattern

--- polls/urls.py
+++ polls/urls.py
@@ -3,4 +3,7 @@ from .                import views
 
 urlpatterns = [
 	url(r'^$', views.index, name = 'index'),
+	url(r'^(?P<question_id>[0-9]+)/$',         views.detail,  name = 'detail'),    # Ex : /polls/5/
+	url(r'^(?P<question_id>[0-9]+)/results/$', views.results, name = 'results'),   # Ex : /polls/5/results/
+	url(r'^(?P<question_id>[0-9]+)/vote/$',    views.vote,    name = 'vote'),      # Ex : /polls/5/vote/
 ]
--- polls/views.py
+++ polls/views.py
@@ -3,3 +3,12 @@ from django.http      import HttpResponse
 
 def index(request):
   return HttpResponse("Hello django! You're on the polls index!")
+
+def detail(request, question_id):
+	return HttpResponse("You are looking question %s." % question_id)
+
+def results(request, question_id):
+	return HttpResponse("You are looking the result of question %s." % question_id)
+
+def vote(request, question_id):
+	return HttpResponse("You are looking the vote of question %s." % question_id)
